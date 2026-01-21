482ac8b0fab0be7faf7a759c67a7b5d6682d7ac9
chaurasiyaanshika81-beep <chaurasiyaanshika81@gmail.com>
2026-01-21 16:57:33 +0000

    comparision.js

---
+++ Basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(4>5);
+// console.log(5>4);
+// console.log(6>=5);
+// console.log(true==true);
+// console.log(true!=false);
+
+
+// console.log(null==0);
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null==undefined);
+// console.log(null<undefined);
+// console.log(null>undefined);
+// console.log(null == null);
+
+
+// ===  strictly operator it check datatype also.
+
+// console.log("45" === 55);
+
+
+
