94970f74f676016fbdfe28184cf020523e619723
chaurasiyaanshika81-beep <chaurasiyaanshika81@gmail.com>
2026-01-21 12:49:13 +0000

    conversionoperation config

--- Basics/03_conversionoperation.js
+++ Basics/03_conversionoperation.js
@@ -2,9 +2,65 @@
 // console.log(typeof price);
 // console.log(typeof(price));
 
-
+// let price = "a"          //NaN
+// let price = 's'         // NaN
+// let price = null       // 0
 // let valueInNumber = Number(price)
-// console.log(typeof valueInNumber);
 // console.log(valueInNumber);
+// console.log(typeof valueInNumber);
 
 
+
+// let isloggedin = ""                //false
+// let isloggedin = "aditya"         //true
+// let isloggedin = [43,56]          // true
+// let isloggedin = ''               // false
+// let isloggedin = 'z'             not know 
+// let booleanIsLoggedIn = Boolean(isloggedin)
+// console.log(booleanIsLoggedIn);
+
+
+
+//--------:>string datatype<:---------
+
+// let somenum = 44           // it will convert to string.
+// let stringnumber = String(somenum)
+// console.log(typeof stringnumber);
+
+
+
+//********************* OPERATIONS ******************
+
+// let value =30
+// let newvalue = -50
+// console.log(newvalue);
+
+
+// let str1 = "aditya"
+// let str1i = " "
+// let str2 = "weds"
+// let str2i = " "
+// let str3 = "paridhi"
+
+// console.log(str1 + str1i + str2 +str2i +str3);
+
+// console.log("1"+4+5+2);
+// console.log(4+3+5+"5");
+// console.log((3+4)*8/2);
+
+
+
+// let num1 , num2 , num3
+
+// num1 = num2
+// num2 = num3
+// num3 = num1 = 8+9
+// console.log(num1);
+// console.log(num3);
+
+
+// let score = 98
+// console.log(++score);
+
+
+//Prefix and postfix in java script , MDN docs
